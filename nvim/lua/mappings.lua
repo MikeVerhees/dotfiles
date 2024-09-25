77ccf3311b788654fbfdf3664a9f82f851beb5a3
Mike Verhees <mail@mikeverhees.com>
2024-09-25 17:50:22 +0200

    remove vstasks mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,4 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map({ "n", "v", "o" }, "<Leader>ta", ":lua require('telescope').extensions.vstask.tasks()<CR>")
-map({ "n", "v", "o" }, "<Leader>tl", ":lua require('telescope').extensions.vstask.launch()<CR>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
