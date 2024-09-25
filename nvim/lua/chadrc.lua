c1c8cfed56b88edbd10e4cbc4aa271b96b7dad77
Mike Verhees <mail@mikeverhees.com>
2024-09-25 16:28:22 +0200

    transparency, italic comments & bigger float term

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,17 +1,27 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "material-darker",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "material-darker",
+  transparency = true,
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+}
+M.term = {
+  float = {
+    relative = "editor",
+    row = 0.2,
+    col = 0.15,
+    width = 0.7,
+    height = 0.6,
+    border = "single",
+  },
 }
 
 return M
