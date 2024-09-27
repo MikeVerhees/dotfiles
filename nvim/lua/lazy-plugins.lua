439ff663221ecbdbe88ccbca93d97e052b4e9547
Mike Verhees <mail@mikeverhees.com>
2024-09-27 16:57:05 +0200

    new nvim base without nvChad dependancy

---
+++ nvim/lua/lazy-plugins.lua
@@ -0,0 +1,47 @@
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  --
+
+  require 'plugins/gitsigns',
+  require 'plugins/which-key',
+  require 'plugins/telescope',
+  require 'plugins/lspconfig',
+  require 'plugins/conform',
+  require 'plugins/cmp',
+  require 'plugins/tokyonight',
+  require 'plugins/todo-comments',
+  require 'plugins/mini',
+  require 'plugins/treesitter',
+  require 'plugins/debug',
+  require 'plugins/indent_line',
+  require 'plugins/lint',
+  require 'plugins/autopairs',
+  require 'plugins/nvim-tree',
+  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,30 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  cmd = 'Nvimtree',
+  keys = {
+    { '\\', ':NvimTreeToggle<CR>', desc = 'Open filetree', silent = true },
+  },
+  opts = {
+    filters = { dotfiles = false },
+    disable_netrw = true,
+    hijack_cursor = true,
+    renderer = {
+      root_folder_label = false,
+      highlight_git = true,
+    },
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
