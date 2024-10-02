31e820c4f2da69a898d4bb3706b366a991811cac
Mike Verhees <mail@mikeverhees.com>
2024-10-02 15:18:51 +0200

    nvim relative linenumbers

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,7 +3,7 @@
 --  For more options, you can see `:help option-list`
 
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
