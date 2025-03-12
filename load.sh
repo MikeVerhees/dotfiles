b6539ad45fd2f1bc8e117c680e7076979cd25aa0
Mike Verhees <mail@mikeverhees.com>
2025-03-12 14:39:02 +0100

    add lazygit conf with history/blame hotkeys & disabled autofetch due to index.lock bug

--- load.sh
+++ load.sh
@@ -3,3 +3,4 @@ ln -sfn ~/Projects/Dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/Projects/Dotfiles/ghostty ~/.config
 ln -sfn ~/Projects/Dotfiles/helix ~/.config
 ln -sfn ~/Projects/Dotfiles/yazi ~/.config
+ln -sfn ~/Projects/Dotfiles/lazygit ~/.config
