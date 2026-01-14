23bdb411512ae117cf2c174ebc38a89d4c55db48
Mike Verhees <mike@verhees.eu>
2026-01-14 14:36:49 +0100

    add fish

--- load.sh
+++ load.sh
@@ -4,3 +4,4 @@ ln -sfn ~/Projects/Dotfiles/ghostty ~/.config
 ln -sfn ~/Projects/Dotfiles/helix ~/.config
 ln -sfn ~/Projects/Dotfiles/yazi ~/.config
 ln -sfn ~/Projects/Dotfiles/lazygit ~/.config
+ln -sfn ~/Projects/Dotfiles/fish ~/.config
